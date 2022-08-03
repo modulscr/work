e0340c4e98b51e835de5620d26639963e2510a74
modulscr <110497273+modulscr@users.noreply.github.com>
2022-08-03 05:08:30 +0500

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ class foxMod(loader.Module):
     strings = {"name": "fox"}
 
     async def watcher(message):
-        if message.raw_text == "Продолжить 🛠 Работать":
+        if "Продолжить 🛠 Работать" in message.raw_text:
             await message.client.send_message("/work")
